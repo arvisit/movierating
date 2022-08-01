eca616ac79f4c5920616fc8ed921033532614ae2
Artem Sitko <sitko.ar@gmail.com>
2022-08-01 21:43:34 +0300

    Add ErrorCommand

---
+++ src/main/java/com/company/movierating/controller/command/impl/ErrorCommand.java
@@ -0,0 +1,20 @@
+package com.company.movierating.controller.command.impl;
+
+import com.company.movierating.controller.command.Command;
+import com.company.movierating.exception.UnsupportedCommandException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class ErrorCommand implements Command {
+
+    public ErrorCommand() {
+    }
+
+    @Override
+    public String execute(HttpServletRequest req) {
+        String msg = "Bad request";
+        req.setAttribute("errorMessage", msg);
+        throw new UnsupportedCommandException(msg);
+    }
+
+}
