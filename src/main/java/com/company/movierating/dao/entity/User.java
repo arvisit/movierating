8dfedf1c4256a3d7157f14900f12dd561baa45a8
Artem Sitko <sitko.ar@gmail.com>
2022-08-01 14:58:04 +0300

    Add entity User

---
+++ src/main/java/com/company/movierating/dao/entity/User.java
@@ -0,0 +1,26 @@
+package com.company.movierating.dao.entity;
+
+import java.time.ZonedDateTime;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String login;
+    private String email;
+    private String password;
+    private Role role;
+    private ZonedDateTime registration;
+
+    public enum Role {
+        ADMIN, USER
+    }
+
+    public void setEmail(String email) {
+        if (!email.matches("[\\w\\.]+@[\\w\\.]+\\.\\w+")) {
+            throw new IllegalArgumentException(email + " is not valid value for email");
+        }
+        this.email = email;
+    }
+}
