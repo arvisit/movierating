3fc309a2d09482baad4958ed7e82939981bae964
Artem Sitko <sitko.ar@gmail.com>
2022-08-01 21:43:51 +0300

    Add UsersCommand

---
+++ src/main/java/com/company/movierating/controller/command/impl/UsersCommand.java
@@ -0,0 +1,25 @@
+package com.company.movierating.controller.command.impl;
+
+import java.util.List;
+
+import com.company.movierating.controller.command.Command;
+import com.company.movierating.service.UserService;
+import com.company.movierating.service.dto.UserDto;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class UsersCommand implements Command {
+    private final UserService service;
+
+    public UsersCommand(UserService service) {
+        this.service = service;
+    }
+
+    @Override
+    public String execute(HttpServletRequest req) {
+        List<UserDto> users;
+        users = service.getAll();
+        req.setAttribute("users", users);
+        return "jsp/user/users.jsp";
+    }
+}
