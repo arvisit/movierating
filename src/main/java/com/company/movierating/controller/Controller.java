da7f66c53bf4820ca8f6a6b10d00d59722f84436
Artem Sitko <sitko.ar@gmail.com>
2022-08-03 02:41:32 +0300

    Modify controller to handle post requests and more exceptions

--- src/main/java/com/company/movierating/controller/Controller.java
+++ src/main/java/com/company/movierating/controller/Controller.java
@@ -5,15 +5,16 @@ import java.io.IOException;
 import com.company.movierating.controller.command.Command;
 import com.company.movierating.controller.command.factory.CommandFactory;
 import com.company.movierating.dao.connection.DataSource;
-import com.company.movierating.exception.ServiceException;
-import com.company.movierating.exception.UnsupportedCommandException;
+import com.company.movierating.exception.controller.RegisterPasswordConfirmationException;
+import com.company.movierating.exception.controller.UnsupportedCommandException;
+import com.company.movierating.exception.service.NoRecordFoundException;
+import com.company.movierating.exception.service.RegistrationValidationException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.xml.bind.ValidationException;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -22,6 +23,11 @@ import lombok.extern.log4j.Log4j2;
 public class Controller extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         process(req, resp);
     }
 
@@ -41,17 +47,22 @@ public class Controller extends HttpServlet {
 
     private String handleException(HttpServletRequest req, HttpServletResponse resp, Exception e) {
         String page;
-        if (e instanceof ValidationException) {
-            req.setAttribute("errorMessage", "Validation error");
-            resp.setStatus(404);
-            page = "jsp/error/404.jsp";
-        } else if (e instanceof ServiceException || e instanceof UnsupportedCommandException) {
+        if (e instanceof UnsupportedCommandException) {
             if (req.getAttribute("errorMessage") == null) {
                 req.setAttribute("errorMessage", e.getMessage());
-//                req.setAttribute("errorMessage", "Service error");
             }
             resp.setStatus(400);
             page = "jsp/error/400.jsp";
+        } else if(e instanceof RegisterPasswordConfirmationException) {
+            req.setAttribute("errorMessage", e.getMessage());
+            page = "jsp/create/create_user_form.jsp";
+        } else if (e instanceof RegistrationValidationException) {
+            req.setAttribute("errorMessage", e.getMessage());
+            page = "jsp/create/create_user_form.jsp";
+        } else if(e instanceof NoRecordFoundException) {
+            req.setAttribute("errorMessage", "Page not found");
+            resp.setStatus(404);
+            page = "jsp/error/404.jsp";
         } else {
             req.setAttribute("errorMessage", "Internal server error");
             resp.setStatus(500);
