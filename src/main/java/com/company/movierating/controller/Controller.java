6b4681e9d4f5472d6d4e4bea2d6650f71b26a171
Artem Sitko <sitko.ar@gmail.com>
2022-08-04 14:41:46 +0300

    Extract error handling to separate class, use default error page

--- src/main/java/com/company/movierating/controller/Controller.java
+++ src/main/java/com/company/movierating/controller/Controller.java
@@ -5,12 +5,6 @@ import java.io.IOException;
 import com.company.movierating.controller.command.Command;
 import com.company.movierating.controller.command.factory.CommandFactory;
 import com.company.movierating.dao.connection.DataSource;
-import com.company.movierating.exception.controller.NonAuthorizedException;
-import com.company.movierating.exception.controller.RegisterPasswordConfirmationException;
-import com.company.movierating.exception.controller.UnsupportedCommandException;
-import com.company.movierating.exception.service.ForbiddenPageException;
-import com.company.movierating.exception.service.NoRecordFoundException;
-import com.company.movierating.exception.service.RegistrationValidationException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -23,6 +17,8 @@ import lombok.extern.log4j.Log4j2;
 @WebServlet("/controller")
 @SuppressWarnings("serial")
 public class Controller extends HttpServlet {
+    private final ExceptionHandler exceptionHandler = ExceptionHandler.INSTANCE;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         process(req, resp);
@@ -42,43 +38,9 @@ public class Controller extends HttpServlet {
             page = commandInstance.execute(req);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
-            page = handleException(req, resp, e);
+            page = exceptionHandler.handleException(req, resp, e);
         }
         req.getRequestDispatcher(page).forward(req, resp);
-    }
-
-    private String handleException(HttpServletRequest req, HttpServletResponse resp, Exception e) {
-        String page;
-        if (e instanceof UnsupportedCommandException) {
-            if (req.getAttribute("errorMessage") == null) {
-                req.setAttribute("errorMessage", e.getMessage());
-            }
-            resp.setStatus(400);
-            page = "jsp/error/400.jsp";
-        } else if (e instanceof RegisterPasswordConfirmationException) {
-            req.setAttribute("errorMessage", e.getMessage());
-            page = "jsp/create/create_user_form.jsp";
-        } else if (e instanceof RegistrationValidationException) {
-            req.setAttribute("errorMessage", e.getMessage());
-            page = "jsp/create/create_user_form.jsp";
-        } else if (e instanceof NonAuthorizedException) {
-            req.setAttribute("errorMessage", e.getMessage());
-            resp.setStatus(401);
-            page = "jsp/sign_in/sign_in_form.jsp";
-        } else if (e instanceof ForbiddenPageException) {
-            req.setAttribute("errorMessage", e.getMessage());
-            resp.setStatus(403);
-            page = "index.jsp";
-        } else if (e instanceof NoRecordFoundException) {
-            req.setAttribute("errorMessage", "Page not found");
-            resp.setStatus(404);
-            page = "jsp/error/404.jsp";
-        } else {
-            req.setAttribute("errorMessage", "Internal server error");
-            resp.setStatus(500);
-            page = "jsp/error/500.jsp";
-        }
-        return page;
     }
 
     @Override
---
+++ src/main/java/com/company/movierating/controller/ExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.company.movierating.controller;
+
+import com.company.movierating.exception.controller.NonAuthorizedException;
+import com.company.movierating.exception.controller.RegisterPasswordConfirmationException;
+import com.company.movierating.exception.controller.UnsupportedCommandException;
+import com.company.movierating.exception.service.ForbiddenPageException;
+import com.company.movierating.exception.service.NoRecordFoundException;
+import com.company.movierating.exception.service.RegistrationValidationException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public enum ExceptionHandler {
+    INSTANCE;
+
+    public String handleException(HttpServletRequest req, HttpServletResponse resp, Exception e) {
+        String page;
+        int status;
+        String message;
+
+        if (e instanceof UnsupportedCommandException) {
+            status = 400;
+            message = e.getMessage();
+            page = "jsp/error/error.jsp";
+        } else if (e instanceof RegisterPasswordConfirmationException) {
+            status = 400;
+            message = e.getMessage();
+            page = "jsp/create/create_user_form.jsp";
+        } else if (e instanceof RegistrationValidationException) {
+            status = 400;
+            message = e.getMessage();
+            page = "jsp/create/create_user_form.jsp";
+        } else if (e instanceof NonAuthorizedException) {
+            status = 401;
+            message = e.getMessage();
+            page = "jsp/sign_in/sign_in_form.jsp";
+        } else if (e instanceof ForbiddenPageException) {
+            status = 403;
+            message = e.getMessage();
+            page = "index.jsp";
+        } else if (e instanceof NoRecordFoundException) {
+            status = 404;
+            message = "Page not found";
+            page = "jsp/error/404.jsp";
+        } else {
+            status = 500;
+            message = "Internal server error";
+            page = "jsp/error/error.jsp";
+        }
+
+        req.setAttribute("errorStatus", status);
+        req.setAttribute("errorMessage", message);
+        resp.setStatus(status);
+
+        return page;
+    }
+}
