ea2bdf7fe9c0dbc1d409e2b552a7620312f1f8ac
Artem Sitko <sitko.ar@gmail.com>
2022-08-10 03:51:58 +0300

    Add util class with constants

---
+++ src/main/java/com/company/movierating/dao/util/Constants.java
@@ -0,0 +1,6 @@
+package com.company.movierating.dao.util;
+
+public abstract class Constants {
+    public static String DB_ZONED_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSSx";
+    public static String APP_ZONED_DATE_TIME_FORMAT = "yyyy-MM-ddTHH:mm:ss.SSSx";
+}
