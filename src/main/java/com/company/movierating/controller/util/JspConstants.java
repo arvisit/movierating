a8d8bcb8953f8b62c72cb30d906ac4f0ed69f4e0
Artem Sitko <sitko.ar@gmail.com>
2022-08-17 00:40:46 +0300

    Add edit ban form page

--- src/main/java/com/company/movierating/controller/util/JspConstants.java
+++ src/main/java/com/company/movierating/controller/util/JspConstants.java
@@ -9,6 +9,7 @@ public abstract class JspConstants {
     public static final String SIGN_IN_FORM = "jsp/sign_in/sign_in_form.jsp";
     public static final String CREATE_USER_FORM = "jsp/create/create_user_form.jsp";
     public static final String EDIT_USER_FORM = "jsp/edit/edit_user_form.jsp";
+    public static final String EDIT_BAN_FORM = "jsp/edit/edit_ban_form.jsp";
     public static final String BAN_USER_FORM = "jsp/edit/ban_user_form.jsp";
     public static final String DEFAULT_ERROR = "jsp/error/error.jsp";
     
