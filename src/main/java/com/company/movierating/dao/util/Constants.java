d540b82efaad7b241b01810750020b5d2b6674f0
Artem Sitko <sitko.ar@gmail.com>
2022-08-17 00:42:12 +0300

    Correct pattern

--- src/main/java/com/company/movierating/dao/util/Constants.java
+++ src/main/java/com/company/movierating/dao/util/Constants.java
@@ -2,5 +2,5 @@ package com.company.movierating.dao.util;
 
 public abstract class Constants {
     public static String DB_ZONED_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSSx";
-    public static String APP_ZONED_DATE_TIME_FORMAT = "yyyy-MM-ddTHH:mm:ss.SSSx";
+    public static String APP_ZONED_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSxxx";
 }
