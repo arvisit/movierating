4436e40f7a69deecdf3d298c9b6da0e6b8cc11c3
Artem Sitko <sitko.ar@gmail.com>
2022-08-18 04:15:29 +0300

    Modify to use exception handler

--- src/main/java/com/company/movierating/controller/filter/AuthorizationFilter.java
+++ src/main/java/com/company/movierating/controller/filter/AuthorizationFilter.java
@@ -2,10 +2,11 @@ package com.company.movierating.controller.filter;
 
 import java.io.IOException;
 
+import com.company.movierating.controller.ExceptionHandler;
 import com.company.movierating.controller.command.factory.CommandFactory;
 import com.company.movierating.controller.command.factory.CommandIdentity;
-import com.company.movierating.controller.util.JspConstants;
 import com.company.movierating.controller.util.SecurityLevel;
+import com.company.movierating.exception.controller.NonAuthorizedException;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -15,6 +16,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 public class AuthorizationFilter extends HttpFilter {
+    private final ExceptionHandler exceptionHandler = ExceptionHandler.INSTANCE;
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
@@ -24,8 +26,9 @@ public class AuthorizationFilter extends HttpFilter {
         if (commandIdentity.getSecurityLevel().getValue() > SecurityLevel.GUEST.getValue()) {
             HttpSession session = req.getSession(false);
             if (session == null || session.getAttribute("user") == null) {
-                req.setAttribute("errorMessage", "Authorization needed");
-                req.getRequestDispatcher(JspConstants.SIGN_IN_FORM).forward(req, res);
+                Exception e = new NonAuthorizedException("Authorization needed");
+                String page = exceptionHandler.handleException(req, res, e);
+                req.getRequestDispatcher(page).forward(req, res);
                 return;
             }
         }
