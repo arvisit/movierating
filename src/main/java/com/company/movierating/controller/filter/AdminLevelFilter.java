826ac461c15807b17c67fdd79f5b4c7d3c9389ee
Artem Sitko <sitko.ar@gmail.com>
2022-08-18 04:16:24 +0300

    Specify filters for different security levels and register them

---
+++ src/main/java/com/company/movierating/controller/filter/AdminLevelFilter.java
@@ -0,0 +1,40 @@
+package com.company.movierating.controller.filter;
+
+import java.io.IOException;
+
+import com.company.movierating.controller.ExceptionHandler;
+import com.company.movierating.controller.command.factory.CommandFactory;
+import com.company.movierating.controller.command.factory.CommandIdentity;
+import com.company.movierating.controller.util.SecurityLevel;
+import com.company.movierating.exception.service.ForbiddenPageException;
+import com.company.movierating.service.dto.UserDto;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class AdminLevelFilter extends HttpFilter {
+    private final ExceptionHandler exceptionHandler = ExceptionHandler.INSTANCE;
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        CommandIdentity commandIdentity = CommandFactory.getInstance().getCommandIdentity(req.getParameter("command"));
+        int commandSecurityLevel = commandIdentity.getSecurityLevel().getValue();
+        if (commandSecurityLevel == SecurityLevel.ADMIN.getValue()) {
+            HttpSession session = req.getSession(false);
+            UserDto sessionUser = (UserDto) session.getAttribute("user");
+            if (sessionUser.getRole() != UserDto.Role.ADMIN) {
+                Exception e = new ForbiddenPageException("Admin rights level is needed");
+                String page = exceptionHandler.handleException(req, res, e);
+                req.getRequestDispatcher(page).forward(req, res);
+                return;
+            }
+        }
+        chain.doFilter(req, res);
+    }
+
+}
