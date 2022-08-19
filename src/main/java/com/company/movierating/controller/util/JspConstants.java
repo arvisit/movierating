1d9d018a271b93d330500e000409a8734ef4862d
Artem Sitko <sitko.ar@gmail.com>
2022-08-20 02:42:38 +0300

    Add attribute names constants

--- src/main/java/com/company/movierating/controller/util/JspConstants.java
+++ src/main/java/com/company/movierating/controller/util/JspConstants.java
@@ -2,6 +2,10 @@ package com.company.movierating.controller.util;
 
 public abstract class JspConstants {
 
+    public static final String ERROR_MESSAGE_ATTRIBUTE_NAME = "errorMessage";
+    public static final String SUCCESS_MESSAGE_ATTRIBUTE_NAME = "successMessage";
+    public static final String LAST_PAGE_ATTRIBUTE_NAME = "lastPage";
+
     public static final String VIEW_USER = "jsp/view/user.jsp";
     public static final String VIEW_USERS = "jsp/view/users.jsp";
     public static final String VIEW_BANS = "jsp/view/bans.jsp";
@@ -12,5 +16,5 @@ public abstract class JspConstants {
     public static final String EDIT_BAN_FORM = "jsp/edit/edit_ban_form.jsp";
     public static final String BAN_USER_FORM = "jsp/edit/ban_user_form.jsp";
     public static final String DEFAULT_ERROR = "jsp/error/error.jsp";
-    
+
 }
