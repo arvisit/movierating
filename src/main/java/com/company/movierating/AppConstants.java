df914b88861a57ae6f5db6dc09279fe623fc252c
Artem Sitko <sitko.ar@gmail.com>
2022-09-07 02:52:38 +0300

    Save image storage properties as constants

---
+++ src/main/java/com/company/movierating/AppConstants.java
@@ -0,0 +1,21 @@
+package com.company.movierating;
+
+import com.company.movierating.dao.connection.ConfigurationManager;
+
+public class AppConstants {
+
+    public static final String DEFAULT_AVATAR_NAME = ConfigurationManager.getInstance().getImageDefaultAvatar();
+    public static final String DEFAULT_POSTER_NAME = ConfigurationManager.getInstance().getImageDefaultPoster();
+
+    public static final String IMAGE_STORAGE_ROOT = ConfigurationManager.getInstance().getImageRootPath();
+    public static final String IMAGE_STORAGE_AVATAR = IMAGE_STORAGE_ROOT + "/avatar";
+    public static final String IMAGE_STORAGE_POSTER = IMAGE_STORAGE_ROOT + "/poster";
+    public static final String DEFAULT_STORAGE_AVATAR = IMAGE_STORAGE_AVATAR + "/" + DEFAULT_AVATAR_NAME;
+    public static final String DEFAULT_STORAGE_POSTER = IMAGE_STORAGE_POSTER + "/" + DEFAULT_POSTER_NAME;
+
+    public static final String IMAGE_APP_ROOT = "img";
+    public static final String IMAGE_APP_AVATAR = IMAGE_APP_ROOT + "/avatar";
+    public static final String IMAGE_APP_POSTER = IMAGE_APP_ROOT + "/poster";
+    public static final String DEFAULT_APP_AVATAR = IMAGE_APP_AVATAR + "/" + DEFAULT_AVATAR_NAME;
+    public static final String DEFAULT_APP_POSTER = IMAGE_APP_POSTER + "/" + DEFAULT_POSTER_NAME;
+}
