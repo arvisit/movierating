5309b897d1b34b487d5e61b6d756dec0192b89da
Artem Sitko <sitko.ar@gmail.com>
2022-09-09 01:40:16 +0300

    Add poster field to film entity

--- src/main/java/com/company/movierating/dao/entity/Film.java
+++ src/main/java/com/company/movierating/dao/entity/Film.java
@@ -10,6 +10,7 @@ public class Film {
     private Integer releaseYear;
     private Integer length;
     private AgeRating ageRating;
+    private String poster;
 
     public enum AgeRating {
         G, PG, PG_13, R, NC_17;
